0c6094c1ea2c295950c8cf0dae69657b4cd74cee
rhan1207 <hanrj1207@yahoo.com>
2018-02-04 16:11:49 -0500

    added data loader for squad

--- QA_env/BiDAF_rep.py
+++ QA_env/BiDAF_rep.py
@@ -1,1 +1,29 @@
 import numpy as np
+from tqdm import tqdm
+import argparse
+import json
+import math
+import os
+import shutil
+from pprint import pprint
+
+from read_data import read_data, get_squad_data_filter, update_config
+from args import parse_args
+
+def main():
+    config = parse_args()
+    data_filter = get_squad_data_filter(config)
+    train_data = read_data(config, 'train', config.load, data_filter=data_filter)
+    count = 0
+    for idx, ds in train_data.get_batches(config.batch_size, num_batches=None, shuffle=False, cluster=False):
+        if count > 1:
+            break
+        for i in idx:
+            print(i)
+            print(ds.data)
+        count += 1
+    return
+
+
+if __name__ == "__main__":
+    main()
---
+++ QA_env/args.py
@@ -0,0 +1,91 @@
+import argparse
+
+def parse_args():
+
+    parser = argparse.ArgumentParser()
+
+    # Name and directories
+    parser.add_argument('-model_name', type=str, default='basic')
+    parser.add_argument('-data_dir', type=str, default='/Users/RJ/data/squad/')
+    parser.add_argument('-run_id', type=int, default=0)
+    parser.add_argument('-out_dir', type=str, default='/Users/RJ/data/squad/')
+    parser.add_argument('-forward_name', type=str, default='single')
+    parser.add_argument('-answer_path', type=str, default='')
+    parser.add_argument('-eval_path', type=str, default='')
+    parser.add_argument('-load_path', type=str, default='')
+    parser.add_argument('-shared_path', type=str, default='/Users/RJ/data/squad/')
+
+    # Device placement
+    parser.add_argument('-use_gpu', type=bool, default=False)
+
+
+    # Essential training and test options
+    parser.add_argument('-mode', type=str, default="test")
+    parser.add_argument('-load', type=bool, default=False)
+    parser.add_argument('-single', type=bool, default=False)
+    parser.add_argument('-debug', type=bool, default=False)
+    parser.add_argument('-load_ema', type=bool, default=True)
+    parser.add_argument('-eval', type=bool, default=True)
+
+    # Traning / test parameters
+    parser.add_argument('-batch_size', type=int, default=2)
+    parser.add_argument('-num_epochs', type=int, default=12)
+    parser.add_argument('-num_steps', type=int, default=20000)
+    parser.add_argument('-init_lr', type=float, default=0.5)
+    parser.add_argument('-input_keep_prob', type=float, default=0.8)
+    parser.add_argument('-keep_prob', type=float, default=0.8)
+    parser.add_argument('-wd', type=float, default=0.0)
+    parser.add_argument('-hidden_size', type=int, default=100)
+    parser.add_argument('-char_out_size', type=int, default=100)
+    parser.add_argument('-char_emb_size', type=int, default=8)
+    parser.add_argument('-out_channel_dims', type=int, default=100)
+    parser.add_argument('-filter_heights', type=int, default=5)
+    parser.add_argument('-finetune', type=bool, default=False)
+    parser.add_argument('-highway', type=bool, default=True)
+    parser.add_argument('-highway_num_layers', type=int, default=2)
+    parser.add_argument('-share_cnn_weights', type=bool, default=True)
+    parser.add_argument('-share_lstm_weights', type=bool, default=True)
+    parser.add_argument('-var_decay', type=float, default=0.999)
+
+    # Logging and saving options
+    parser.add_argument('-progress', type=bool, default=True)
+    parser.add_argument('-log_period', type=int, default=100)
+    parser.add_argument('-eval_period', type=int, default=1000)
+    parser.add_argument('-save_period', type=int, default=1000)
+    parser.add_argument('-max_to_keep', type=int, default=20)
+    parser.add_argument('-dump_eval', type=bool, default=True)
+    parser.add_argument('-dump_answer', type=bool, default=True)
+    parser.add_argument('-vis', type=bool, default=False)
+    parser.add_argument('-dump_pickle', type=bool, default=True)
+    parser.add_argument('-decay', type=float, default=0.9)
+
+    # Thresholds for speed and less memory usage
+    parser.add_argument('-word_count_th', type=int, default=10)
+    parser.add_argument('-char_count_th', type=int, default=50)
+    parser.add_argument('-sent_size_th', type=int, default=400)
+    parser.add_argument('-num_sents_th', type=int, default=8)
+    parser.add_argument('-ques_size_th', type=int, default=30)
+    parser.add_argument('-word_size_th', type=int, default=16)
+    parser.add_argument('-para_size_th', type=int, default=256)
+
+    # Advanced training options
+    parser.add_argument('-lower_word', type=bool, default=True)
+    parser.add_argument('-squash', type=bool, default=False)
+    parser.add_argument('-swap_memory', type=bool, default=True)
+    parser.add_argument('-data_filter', type=str, default="max")
+    parser.add_argument('-use_glove_for_unk', type=bool, default=True)
+    parser.add_argument('-known_if_glove', type=bool, default=True)
+    parser.add_argument('-logit_func', type=str, default="tri_linear")
+    parser.add_argument('-answer_func', type=str, default="linear")
+    parser.add_argument('-sh_logit_func', type=str, default="tri_linear")
+
+    # Ablation options
+    parser.add_argument('-use_char_emb', type=bool, default=True)
+    parser.add_argument('-use_word_emb', type=bool, default=True)
+    parser.add_argument('-q2c_att', type=bool, default=True)
+    parser.add_argument('-c2q_att', type=bool, default=True)
+    parser.add_argument('-dynamic_att', type=bool, default=False)
+
+    args = parser.parse_args()
+
+    return args
