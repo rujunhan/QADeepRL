13f242261bfe2f8c8bb45ac0816e8f691bc956ab
rhan1207 <hanrj1207@yahoo.com>
2018-03-24 15:19:08 -0400

    datastream

--- NMT/datastream.py
+++ NMT/datastream.py
@@ -1,4 +1,3 @@
-import numpy as np
 import torch
 import torch.autograd as autograd
 import torch.nn as nn
@@ -16,37 +15,36 @@ class load_data():
     def __init__(self, args):
 
         self.batch_size = args.batch_size
-        #self.data_size = args.data_size
+        self.max_sent_len = args.max_sent_len
 
-        #self.vocab = np.load(args.source+args.vocab).item()
-
+        #self.vocab = np.load(args.source+args.vocab).item()                                                                                     
         fname = args.source+args.data_file
-        print(fname)
+
         self.source = open(fname, 'r')
 
         self.end_of_data = False
 
         self.count = 0
 
-
     def reset(self):
         self.source.seek(0)
         self.count = 0
         raise StopIteration
-
 
     def __iter__(self):
         return(self)
 
     def __next__(self):
 
-        data = []
         count = 0
+
+        batch_s = []
+        batch_t = []
 
         while True:
             line = self.source.readline().strip().split(',')
-            source = line[0]
-            target = line[1]
+            source = line[0].split(' ')
+            target = line[1].split(' ')
 
             if line == '':
                 print("end of file!")
@@ -55,28 +53,33 @@ class load_data():
 
             self.count += 1
 
-            #label 
-            #if len(line) < 2:
-            #    continue
             count += 1
-            #text_list = text.split(" ")
+                       batch_s.append(source)
+            batch_t.append(target)
 
-            #elif label in self.labels:
-            #    count += 1
-
-            #for i in range(self.skips, len(text_list) - self.skips):
-
-            #    out_text = text_list[i-self.skips:i] + text_list[i+1:i+self.skips+1]
-
-            #    in_text = text_list[i]
-
-            #    data.append((label, in_text, out_text))
-            data.append(line)
             if count >= self.batch_size:
                 break
-            
-        #in_idxs, out_idxs, covars = self.create_batch(data, self.vocab)
-        
-        #return in_idxs, out_idxs, covars
 
-        return data
+        batch_s = self.mask_sent(batch_s)
+        batch_t = self.mask_sent(batch_t)
+
+        return Variable(torch.LongTensor(batch_s), requires_grad=False), Variable(torch.LongTensor(batch_t), requires_grad=False)
+
+
+    def mask_sent(self, raw_batch):
+
+        max_sent_len = max([len(x) for x in raw_batch])
+
+        batch = []
+        for sent in raw_batch:
+
+            sent = [int(x) for x in sent]
+
+            if len(sent) >= max_sent_len:
+                sent = sent[:max_sent_len]
+            else:
+                sent.extend([int(x) for x in np.zeros(max_sent_len - len(sent))])
+
+            batch.append(sent)
+
+        return np.stack(batch, axis=0)
